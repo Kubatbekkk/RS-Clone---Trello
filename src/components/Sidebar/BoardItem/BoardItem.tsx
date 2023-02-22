f842d27b759de9dcbd3f0b82d9108b0eeaf4b4fa
Kubatbekkk <kubineccastro@gmail.com>
2023-02-23 01:36:54 +0600

    feat: add BoardItem, BoardsList

--- src/components/Sidebar/BoardItem/BoardItem.tsx
+++ src/components/Sidebar/BoardItem/BoardItem.tsx
@@ -1,7 +1,27 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import NotesIcon from 'src/assets/notes-icon.svg';
+import { useNavigate } from 'react-router-dom';
+import { StyledBoardItem } from './styles';
 
-const BoardItem = () => (
-  <div>BoardItem</div>
-);
+interface ItemProps {
+  props: {
+    id: string;
+    name: string;
+  }
+}
+
+const BoardItem = ({ props }: ItemProps) => {
+  const { id, name } = props;
+  const navigate = useNavigate();
+  return (
+    <StyledBoardItem active onClick={() => navigate(`/board/${id}`)}>
+      <div>
+        <img src={NotesIcon} alt="notes" />
+        <p>{name}</p>
+      </div>
+    </StyledBoardItem>
+  );
+};
 
 export default BoardItem;
--- src/components/Sidebar/BoardList/BoardList.tsx
+++ src/components/Sidebar/BoardList/BoardList.tsx
@@ -1,8 +1,30 @@
+/* eslint-disable @typescript-eslint/no-redeclare */
 import React from 'react';
+import IBoardItemProps from '../BoardItem';
 import { StyledBoardsList } from './styles';
 
+interface IBoardItemProps {
+  ownerId: string,
+  id: string,
+  name: string
+}
+
+const boards: IBoardItemProps[] = [
+  { ownerId: 'kubat', id: '1', name: 'Board1' },
+  { ownerId: 'kubat', id: '2', name: 'Board2' },
+];
+
 const BoardList = () => (
-  <StyledBoardsList>BoardList</StyledBoardsList>
+  <StyledBoardsList>
+    <>
+      <h2>Your boards</h2>
+      {boards && boards.length > 0 ? (
+        boards.map((board: IBoardItemProps) => (
+          <IBoardItemProps key={board.id} props={board} />
+        ))
+      ) : (<h2 className='createNewBoard'>Create new board</h2>)}
+    </>
+  </StyledBoardsList>
 );
 
 export default BoardList;
