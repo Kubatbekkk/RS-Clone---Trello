837ff499b38f4caad2bf3619bfcd926422a14064
Kubatbekkk <kubineccastro@gmail.com>
2023-02-28 02:24:52 +0600

    upd: commit before build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
-  devtool: 'source-map',
+  // devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
   },
